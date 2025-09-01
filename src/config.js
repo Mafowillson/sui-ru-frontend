df31ba60f17fc7998af28220ce75a549e3b6069a
mafowillson <fouotsawilson@gmail.com>
2025-09-01 04:18:42 +0100

    migrating to render

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // Central place for API base URL
 
 // export const BASE_API_URL = "https://server.sui-ru.com";
-export const BASE_API_URL = "http://84.247.168.4:8000";
+export const BASE_API_URL = "https://suirubackend.onrender.com";
